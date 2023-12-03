28b0da15522c387753f3beccccb96fccc0ce0c99
Rahman-NMR <49480009+Rahman-NMR@users.noreply.github.com>
2023-12-03 23:12:13 +0700

    v1.0.0.4[part 4] - beranda; remove onDestroyView binding null, replace to isAdded?;

--- app/src/main/java/com/bekado/bekadoonline/ui/beranda/BerandaFragment.kt
+++ app/src/main/java/com/bekado/bekadoonline/ui/beranda/BerandaFragment.kt
@@ -5,19 +5,180 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bekado.bekadoonline.R
+import com.bekado.bekadoonline.adapter.AdapterButton
+import com.bekado.bekadoonline.adapter.AdapterProduk
+import com.bekado.bekadoonline.bottomsheet.SortProdukBottomSheet
 import com.bekado.bekadoonline.databinding.FragmentBerandaBinding
+import com.bekado.bekadoonline.helper.GridSpacingItemDecoration
+import com.bekado.bekadoonline.helper.Helper.calculateSpanCount
+import com.bekado.bekadoonline.helper.HelperConnection
+import com.bekado.bekadoonline.helper.HelperProduk.getAllProduk
+import com.bekado.bekadoonline.helper.HelperProduk.getFiltered
+import com.bekado.bekadoonline.helper.HorizontalSpacingItemDecoration
+import com.bekado.bekadoonline.model.ButtonModel
+import com.bekado.bekadoonline.model.KategoriModel
+import com.bekado.bekadoonline.model.ProdukModel
+import com.bekado.bekadoonline.shimmer.ShimmerModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class BerandaFragment : Fragment() {
-    private var _binding: FragmentBerandaBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentBerandaBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
+    private lateinit var adapterButton: AdapterButton
+    private lateinit var adapterProduk: AdapterProduk
+    private val dataButton: ArrayList<ButtonModel> = ArrayList()
+    private var dataKategori: ArrayList<KategoriModel> = ArrayList()
+    private val dataProduk: ArrayList<ProdukModel> = ArrayList()
+    private val dataShimmer: ArrayList<ShimmerModel> = ArrayList()
+
+    private lateinit var produkRef: DatabaseReference
+    private lateinit var produkListener: ValueEventListener
+
+    var sortFilter = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentBerandaBinding.inflate(inflater, container, false)
+        binding = FragmentBerandaBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseDatabase.getInstance()
+        produkRef = db.getReference("produk")
+
+        val paddingBottom = resources.getDimensionPixelSize(R.dimen.normalBottomdp)
+        val padding = resources.getDimensionPixelSize(R.dimen.smalldp)
+        val lmProduk = GridLayoutManager(context, calculateSpanCount(requireContext()))
+        val lmButton = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        val lmShimmer = GridLayoutManager(context, calculateSpanCount(requireContext()))
+
+        getDataAllProduk()
+        fabScrollToTop()
+        HelperConnection.shimmerProduk(lmShimmer, binding.rvProdukShimmer, padding, dataShimmer)
+
+        with(binding) {
+            swipeRefresh.setOnRefreshListener {
+                if (HelperConnection.isConnected(requireContext())) getDataAllProduk()
+                binding.swipeRefresh.isRefreshing = false
+            }
+            btnSort.setOnClickListener { openBottomSheetSort() }
+            rvProduk.apply {
+                layoutManager = lmProduk
+                addItemDecoration(GridSpacingItemDecoration(lmProduk.spanCount, padding, true))
+                setPadding(0, 0, 0, paddingBottom)
+            }
+            rvButtonSelector.apply {
+                addItemDecoration(HorizontalSpacingItemDecoration(padding))
+                layoutManager = lmButton
+            }
+        }
+    }
+
+    private fun openBottomSheetSort() {
+        val sortBottomSheet = SortProdukBottomSheet(requireContext())
+        sortBottomSheet.showDialog(requireContext(), sortFilter)
+
+        sortBottomSheet.dialog.setOnDismissListener {
+            sortFilter = sortBottomSheet.sortFilter
+            getDataAllProduk()
+        }
+    }
+
+    private fun fabScrollToTop() {
+        val fab = binding.scrollToTop
+        val rvScrolling = binding.rvProduk
+        var isFabVisible = false
+
+        fab.visibility = View.INVISIBLE
+        rvScrolling.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0 && !isFabVisible) {
+                    fab.show()
+                    isFabVisible = true
+                } else if (dy < 0 && isFabVisible) {
+                    fab.hide()
+                    isFabVisible = false
+                }
+            }
+        })
+
+        fab.setOnClickListener {
+            rvScrolling.smoothScrollToPosition(0)
+        }
+    }
+
+    private fun getDataAllProduk() {
+        produkListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                dataProduk.clear()
+                dataButton.clear()
+
+                val semua = "Semua"
+                dataButton.add(ButtonModel(semua, "", true, 0))
+                if (isAdded) {
+                    for (item in snapshot.child("kategori").children) {
+                        val namaKategori = item.child("namaKategori").value as String
+                        val posisi = item.child("posisi").value as Long
+                        val idKategori = item.child("idKategori").value as String
+                        val kategori = ButtonModel(namaKategori, idKategori, false, posisi)
+
+                        val visibilitas = item.child("visibilitas").value as Boolean
+                        if (visibilitas) dataButton.add(kategori)
+                        dataButton.sortByDescending { it.posisi }
+                        dataButton.reverse()
+
+                        adapterButton = AdapterButton(dataButton) { button ->
+                            if (button.isActive) {
+                                if (button.namaKategori == semua)
+                                    getAllProduk(snapshot, dataProduk, dataKategori, adapterProduk, sortFilter)
+                                else getFiltered(snapshot, button.idKategori, dataProduk, adapterProduk, sortFilter)
+                            }
+                        }
+                        binding.rvButtonSelector.adapter = adapterButton
+                    }
+                }
+                adapterProduk = AdapterProduk(dataProduk) { }
+                getAllProduk(snapshot, dataProduk, dataKategori, adapterProduk, sortFilter)
+                binding.rvProduk.adapter = adapterProduk
+
+                with(binding) {
+                    shimmerRvProduk.apply {
+                        stopShimmer()
+                        visibility = View.GONE
+                    }
+                    shimmerRvButtonSelector.apply {
+                        stopShimmer()
+                        visibility = View.GONE
+                    }
+
+                    rvProduk.visibility = View.VISIBLE
+                    rvButtonSelector.visibility = View.VISIBLE
+                    llBtnSort.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                with(binding) {
+                    shimmerRvProduk.startShimmer()
+                    shimmerRvButtonSelector.startShimmer()
+                    rvProduk.visibility = View.GONE
+                    rvButtonSelector.visibility = View.GONE
+                    llBtnSort.visibility = View.GONE
+                }
+            }
+        }
+        produkRef.addListenerForSingleValueEvent(produkListener)
     }
 }
