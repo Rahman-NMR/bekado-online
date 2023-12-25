322d382d586ffe075b52254f5c51ed1877108674
Rahman-NMR <49480009+Rahman-NMR@users.noreply.github.com>
2023-12-26 01:07:37 +0700

    v1.0.1.16[part 1] - adjust layout inputType;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.1.16"
+        versionName = "1.0.1.17"
 //        big update
 //        feature
 //        stable
