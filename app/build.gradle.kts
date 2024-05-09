ded083a38028160bdbfb7a40c74d47a23d1a96b1
Rahman-NMR <49480009+Rahman-NMR@users.noreply.github.com>
2024-05-09 19:25:48 +0700

    v1.1.1.5 - onClick Animation; remove(cardview_pressed.xml);

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 1
-        versionName = "1.1.1.4"
+        versionName = "1.1.1.5"
 //        big update
 //        sprint
 //        stable
