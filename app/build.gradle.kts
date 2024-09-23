acc5a05e68bb1fb59edae49d9e230963e2208a9b
Rahman-NMR <49480009+Rahman-NMR@users.noreply.github.com>
2024-09-23 21:07:33 +0700

    v1.1.4.11 - cardCornerRadius; adjust layout CardView cardCornerRadius from 99dp to 16dp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 2
-        versionName = "1.1.4.10"
+        versionName = "1.1.4.11"
 //        big update
 //        sprint
 //        stable
