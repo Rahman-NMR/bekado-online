f2553cf01050842146c315217bc6c9d11d47b36b
Rahman-NMR <49480009+Rahman-NMR@users.noreply.github.com>
2024-11-01 20:37:03 +0700

    simplify AboutBekadoActivity.kt

--- app/src/main/java/com/bekado/bekadoonline/view/ui/profil/AboutBekadoActivity.kt
+++ app/src/main/java/com/bekado/bekadoonline/view/ui/profil/AboutBekadoActivity.kt
@@ -5,32 +5,28 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bekado.bekadoonline.R
 import com.bekado.bekadoonline.databinding.ActivityAboutBekadoBinding
-import com.bekado.bekadoonline.helper.Helper.showToast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.firestore.FirebaseFirestore
 
 class AboutBekadoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAboutBekadoBinding
-    private lateinit var fs: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAboutBekadoBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         supportActionBar?.hide()
 
-        fs = FirebaseFirestore.getInstance()
-
         binding.appBar.setNavigationOnClickListener { finish() }
-        getDataToko()
+        with(binding) { getDataToko() }
     }
 
-    private fun getDataToko() {
-        val aboutCol = fs.collection("bekado")
-        aboutCol.document("aboutBekado").get().addOnSuccessListener {
-            with(binding) {
+    private fun ActivityAboutBekadoBinding.getDataToko() {
+        FirebaseFirestore.getInstance()
+            .collection("bekado")
+            .document("aboutBekado").get()
+            .addOnSuccessListener {
                 Glide.with(this@AboutBekadoActivity).load(it.get("fotoToko").toString())
                     .apply(RequestOptions()).centerCrop()
                     .placeholder(R.drawable.img_placeholder)
@@ -45,14 +41,11 @@ class AboutBekadoActivity : AppCompatActivity() {
                 layoutInfoBekado.visibility = View.VISIBLE
                 progressbarFotoBekado.visibility = View.GONE
                 progressbarInfoBekado.visibility = View.GONE
+            }.addOnFailureListener {
+                layoutFotoBekado.visibility = View.GONE
+                layoutInfoBekado.visibility = View.GONE
+                progressbarFotoBekado.visibility = View.VISIBLE
+                progressbarInfoBekado.visibility = View.VISIBLE
             }
-        }.addOnFailureListener {
-            binding.layoutFotoBekado.visibility = View.GONE
-            binding.layoutInfoBekado.visibility = View.GONE
-            binding.progressbarFotoBekado.visibility = View.VISIBLE
-            binding.progressbarInfoBekado.visibility = View.VISIBLE
-
-            showToast(it.toString(), this)
-        }
     }
 }
